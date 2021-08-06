4afdaf7f65238256c40e215b703178827691cc3c
riaz111 <riazasim111@gmail.com>
2021-08-06 13:28:33 +0200

    header component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
